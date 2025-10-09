a88aab237026993f2637870be02f739122474b21
my_first_github_repo <foysaliqbalutsob2202113>
2025-10-09 22:49:13 +0600

    loading page is set

--- src/Component/Root/Root.jsx
+++ src/Component/Root/Root.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
+import LoadingAPI from '../LoadingAPI/LoadingAPI';
 
 const Root = () => {
+
+    const navigation = useNavigation();
+    const [loading, setLoading] =useState(false)
+
+
+    useEffect(() => {
+    if (navigation.state === "loading") {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+  }, [navigation.state]);
+
+
+
     return (
         <div className='flex flex-col min-h-screen '>
             <Header></Header>
+            {
+                loading ? (<LoadingAPI></LoadingAPI>):
+            
+            
+            
             <div className='flex-1'>
                 <Outlet></Outlet>
             </div>
+}
             <Footer></Footer>
+
             
         </div>
+            
     );
 };
 
